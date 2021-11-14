e882caf6b9e75fba0cbf0e5560708a3a3fb0ef6b
XiaogeS1024 <76575906+XiaogeS1024@users.noreply.github.com>
2021-11-14 18:48:11 +0800

    Login Form

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
   <div id="app">
+    <vue-particles
+     color="#409EFF"
+     :particleOpacity="0.7"
+     :particlesNumber="100"
+     shapeType="circle"
+     :particleSize="4"
+     linesColor="#409EFF"
+     :linesWidth="1"
+     :lineLinked="true"
+     :lineOpacity="0.4"
+     :linesDistance="150"
+     :moveSpeed="3"
+     :hoverEffect="true"
+     hoverMode="grab"
+     :clickEffect="true"
+     clickMode="push">
+    </vue-particles>
     <router-view></router-view>
   </div>
 </template>
@@ -11,4 +28,9 @@ export default {
 </script>
 
 <style>
+#particles-js {
+  width: 100%;
+  height: calc(100% - 100px);
+  position: absolute;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import VueParticles from 'vue-particles'
 
+Vue.use(VueParticles)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
