072bf822511841a46bc5cc619ab18a61f56ebbe1
Charles Gao <1030587461@qq.com>
2021-11-23 15:37:43 +0800

    test from GYF

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Login from '../components/LoginForm.vue'
 import AdminLogin from '../components/admin/AdminLogin.vue'
 import Reg from '../components/Register.vue'
 import PwdRecover from '../components/PasswordRecover.vue'
-import AdminHome from '../components/admin/AdminHome.vue'
+// import AdminHome from '../components/admin/AdminHome.vue'
 
 Vue.use(VueRouter)
 
@@ -13,8 +13,8 @@ const routes = [
   { path: '/login', component: Login },
   { path: '/adminLogin', component: AdminLogin },
   { path: '/reg', component: Reg },
-  { path: '/pwdRecover', component: PwdRecover },
-  { path: '/adminHome', component: AdminHome }
+  { path: '/pwdRecover', component: PwdRecover }
+  // { path: '/adminHome', component: AdminHome }
 ]
 
 const router = new VueRouter({
