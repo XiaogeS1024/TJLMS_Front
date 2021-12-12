cf82a32861978709557e4c7281d60d1029c181c2
Charles Gao <1030587461@qq.com>
2021-12-12 11:15:00 +0800

    notice script

--- src/views/stu/stuNotice.vue
+++ src/views/stu/stuNotice.vue
@@ -4,8 +4,48 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-  name: 'stuNotice'
+  name: 'stuNotice',
+  data () {
+    return {
+      titleList: [],
+      queryId: 0,
+      notice: {
+        releaser: '',
+        title: '',
+        content: '',
+        releaseTime: ''
+      },
+      errMsg: ''
+    }
+  },
+  methods: {
+    async getTitles () {
+      const url = '/get/titles'
+      await axios.get(url)
+        .then(
+          (response) => {
+            this.titleList = response.data
+          }
+        )
+        .catch(
+          (err) => {
+            this.errMsg = '暂无公告'
+            console.log(err)
+          }
+        )
+    },
+    async getContent () {
+      const url = '/get/notice/id/' + this.queryId
+      await axios.get(url)
+        .then(
+          (response) => {
+            this.notice.content = response.data.content
+          }
+        )
+    }
+  }
 }
 </script>
 
