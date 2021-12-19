2f1dff971f387da9888bee984ec0fcfcd7452d38
Charles Gao <1030587461@qq.com>
2021-12-19 09:49:17 +0800

    feedback API invoking completed

--- src/views/stu/stuFeedback.vue
+++ src/views/stu/stuFeedback.vue
@@ -5,8 +5,75 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'stuFeedback'
+  name: 'feedback',
+  data: () => ({
+    feedbackForm:
+      {
+        title: '',
+        content: '',
+        isAnonymous: false
+      },
+    pageNum: 1,
+    pageSize: 20,
+    myFeedback: {},
+    errMsg: ''
+
+  }),
+  methods: {
+    feedback () {
+      if (!this.feedbackForm.title) {
+        this.$message.error('请输入反馈标题')
+      } else if (!this.feedbackForm.content) {
+        this.$message.error('请输入反馈内容')
+      } else {
+        this.handleFeedback()
+      }
+    },
+
+    async handleFeedback () {
+      const url = '/post/feedback'
+      const email = sessionStorage.getItem('email')
+      const obj = JSON.parse(sessionStorage.getItem(email))
+      await axios.post(url, {
+        content: this.feedbackForm.content,
+        feedbacker: obj.id,
+        isAnonymous: this.feedbackForm.isAnonymous,
+        title: this.feedbackForm.title
+
+      })
+        .then(
+          (response) => {
+            this.$message.success('反馈提交成功')
+            console.log(response)
+          }
+        )
+        .catch(
+          (err) => {
+            this.$message.error('反馈提交失败')
+            console.log(err)
+          }
+        )
+    },
+    async getMyFeedback () {
+      const email = sessionStorage.getItem('email')
+      const obj = JSON.parse(sessionStorage.getItem(email))
+      const url = '/get/my/reply?id=' + obj.id + '&pageNum=' + this.pageNum + '&pageSize=' + this.pageSize
+      await axios.get(url)
+        .then(
+          (response) => {
+            this.myFeedback = response.data
+          }
+        )
+        .catch(
+          (err) => {
+            this.errMsg = '暂无发出的反馈'
+            console.log(err)
+          }
+        )
+    }
+  }
 }
 </script>
 
--- src/views/teacher/teacherFeedback.vue
+++ src/views/teacher/teacherFeedback.vue
@@ -5,8 +5,88 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'teacherFeedback'
+  name: 'teacherFeedback',
+  data: () => ({
+    replyForm: {
+      title: '',
+      content: ''
+    },
+    pageNum: 1,
+    pageSize: 20,
+    myReply: [],
+    allFeedback: [],
+    errMsg: ''
+  }),
+  methods: {
+    reply (id) {
+      if (!this.replyForm.title) {
+        this.$message.error('请输入回复标题')
+      } else if (!this.replyForm.content) {
+        this.$message.error('请输入回复内容')
+      } else {
+        this.handleReply(id)
+      }
+    },
+    async handleReply (feedbackId) {
+      const url = '/post/reply'
+      const email = sessionStorage.getItem('email')
+      const obj = JSON.parse(sessionStorage.getItem(email))
+      await axios.post(url, {
+        content: this.replyForm.content,
+        id: feedbackId,
+        replier: obj.id,
+        title: this.replyForm.title
+      })
+        .then(
+          (response) => {
+            this.$message.success('回复成功')
+            console.log(response)
+          }
+        )
+        .catch(
+          (err) => {
+            this.$message.error('回复失败')
+            console.log(err)
+          }
+        )
+    },
+    async getMyReply () {
+      const email = sessionStorage.getItem('email')
+      const obj = JSON.parse(sessionStorage.getItem(email))
+      const url = '/get/my/reply?id=' + obj.id + '&pageNum=' + this.pageNum + '&pageSize=' + this.pageSize
+      await axios.get(url)
+        .then(
+          (response) => {
+            this.myReply = response.data
+          }
+        )
+        .catch(
+          (err) => {
+            this.errMsg = '暂无您已回复的反馈'
+            console.log(err)
+          }
+        )
+    },
+    async getAllFeedback () {
+      const url = '/get/all/feedback'
+      await axios.get(url)
+        .then(
+          (response) => {
+            this.allFeedback = response.data.content
+          }
+        )
+        .catch(
+          (err) => {
+            this.errMsg = '暂无未回复的反馈'
+            console.log(err)
+          }
+        )
+    }
+  }
+
 }
 </script>
 
